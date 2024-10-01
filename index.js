dfb05990ed9e238fb2fb6a251d9393a163e4fa14
tarundh <tarundhillon14330@gmail.com>
2024-10-01 08:28:04 +0530

    0.0

---
+++ index.js
@@ -0,0 +1,67 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+app.get('/',(request,response) => {
+    response.send('<h1>Hello world </h1>')
+})
+
+app.get('/api/notes', (request,response) => {
+    response.json(notes)
+})
+
+app.get('/api/notes/:id', (request,response) =>{
+  const id = request.params.id
+  const note = notes.find(n => n.id ===id)
+  if(note){
+    response.json(note)
+  }
+  else{
+    response.status(404).end()
+  }
+
+})
+
+app.delete('/api/notes/:id', (request,response) => {
+  const id = request.params.id
+  notes = notes.filter(n => n.id!==id)
+  response.status(204).end() //204 means no more opertions left to be done 
+})
+
+app.post('/api/notes', (request,response) => {
+  const maxId = notes.length>0 ? Math.max(...notes.map(n => Number(n.id))) : 0 
+  
+  const body = request.body
+  if(!body.content){
+    return response
+  }
+  note.id = String(maxId+1)
+  notes = notes.concat(note)
+  response.json(note)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`server is runnig on port ${PORT}`)
+})
