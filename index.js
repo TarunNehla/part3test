80829c98317afa0eef3c91f15a9dff298f44be95
tarundh <tarundhillon14330@gmail.com>
2024-10-17 19:52:32 +0530

    0.5

--- index.js
+++ index.js
@@ -9,24 +9,6 @@ app.use(express.json())
 
 
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
 
 
 app.get('/',(request,response) => {
